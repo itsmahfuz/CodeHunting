4d71181e7527552dc788e524955003ba76dc4f9a
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-10-01 15:05:14 +0600

    first

---
+++ src/ValidParentheses.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author Mahfuzur Rahman
+ * @Date 10/1/2024
+ */
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ValidParentheses v = new ValidParentheses();
+        v.isValid("()[]{}");
+    }
+
+}
