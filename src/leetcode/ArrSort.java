a37ce5fb25f1ef5d44ee25ba256974c7fb6c0973
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-11-05 18:18:33 +0600

    arr sort implemented

---
+++ src/leetcode/ArrSort.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ArrSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[] {6, 5, 18, 10, 7, 4, 9};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
