e0c1cf8e5c1131f5e894b41e6cbdb70b6f870aa0
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-11-10 15:33:15 +0600

    10.11.25

--- src/leetcode/RemoveElement.java
+++ src/leetcode/RemoveElement.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
@@ -20,9 +17,10 @@ public class RemoveElement {
 
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3,2,2,3};
+        int[] nums = new int[]{3, 2, 2, 3};
         int val = 3;
-        System.out.println(removeElement(nums, val));;
+        System.out.println(removeElement(nums, val));
+        ;
     }
 
 }
